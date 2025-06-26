8695d6baa23306824ca0031879c5397c3e3d549a
Vincent Anim-Addo <vincentanim77@gmail.com>
2025-06-26 11:18:29 -0700

    Navbar feature has been added

--- frontend-new/src/App.jsx
+++ frontend-new/src/App.jsx
@@ -3,21 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import Navbar from './components/Navbar';
 
 function App() {
-return (
+  return (
     <Router>
-   <Routes>
+      <Navbar />
+      <Routes>
         <Route path="/" element={<Home />} />
-         <Route path="/register" element={<Register />} />
-         <Route path="/login" element={<Login />} />
-    </Routes>
-        </Router>
-   // <div>
-     //   <h1>React Router Example</h1>
-   // </div>
-);
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
-
 
 export default App;
--- frontend-new/src/pages/Home.jsx
+++ frontend-new/src/pages/Home.jsx
@@ -1,6 +1,8 @@
-import  React from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 const Home = () => {
-    return <h1> Welcome to Recipe Buddy</h1>;
+  return <h1> Welcome to Recipe Buddy</h1>;
 };
 
 export default Home;
